3ef155a3688465369aad54f42f42b62cc647af1d
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-08-15 11:52:45 +0000

    Task 0

---
+++ 0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' This file houses a function that takes two floats as
+  argument and returns the sum as a float too'''
+
+
+def add(a: float, b: float) -> float:
+    ''' Function for adding floats and returning float value '''
+    return a + b
---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Variable Annotations
+Here, I'll be learning how to make my code more readable by using variable annotations, specifying the types of variables, and functions with their return values
