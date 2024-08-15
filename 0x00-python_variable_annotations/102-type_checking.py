0d7caba46297d6bc980d50d32200b13c001f211e
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-08-15 14:55:12 +0000

    Added description to fuction of 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    ''' Function to be corrected '''
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
