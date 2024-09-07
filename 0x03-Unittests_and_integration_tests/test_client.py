20da65700a66fbea9e59dc101d0fe81eae86c1f6
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-07 13:25:57 +0000

    Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ from client import GithubOrgClient
 from parameterized import parameterized
 from typing import Mapping, Sequence, Dict
 from utils import access_nested_map, get_json, memoize
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 import unittest
 
 
@@ -21,3 +21,17 @@ class TestGithubOrgClient(unittest.TestCase):
         result = GithubOrgClient(a)
         self.assertEqual(result.org, res)
         mock_get.assert_called_once_with(f"https://api.github.com/orgs/{a}")
+
+    def test_public_repos_url(self) -> None:
+        """ Function to test public_repos_url """
+        payload = {
+            "repos_url": "https://api.github.com/orgs/repos_url/repos"}
+        with patch(
+            "client.GithubOrgClient.org", new_callable=PropertyMock
+        ) as mock_method:
+            mock_method.return_value = payload
+            res = GithubOrgClient("repos_url")
+            self.assertEqual(
+                res._public_repos_url,
+                "https://api.github.com/orgs/repos_url/repos"
+            )
