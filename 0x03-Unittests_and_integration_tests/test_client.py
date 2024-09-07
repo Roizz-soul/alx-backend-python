96a990880021d582246375f09790ef1926bce91c
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-07 17:02:58 +0000

    Check 8

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -83,16 +83,6 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
-    def test_public_repos(self) -> None:
-        """ Test public repo"""
-        res = GithubOrgClient("google")
-        self.assertEqual(res.public_repos(), self.c)
-
-    def test_public_repos_with_license(self) -> None:
-        """Test public repos with license"""
-        res = GithubOrgClient("google")
-        self.assertEqual(res.public_repos(license="apache-2.0"), self.d)
-
     @classmethod
     def tearDownClass(cls) -> None:
         """ TearDown Function """
